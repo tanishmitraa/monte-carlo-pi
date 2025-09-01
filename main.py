49796d74d389eab9087f55d074db4fc65d1194e9
Tanish Mitra <101736317+tanishmitraa@users.noreply.github.com>
2025-09-01 15:11:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import numpy as np
 
 def monte_carlo_pi(N):
-    x = np.random.uniform(-1,1,N)
-    y = np.random.uniform(-1,1,N)
+    x = np.random.uniform(-1, 1, N)
+    y = np.random.uniform(-1, 1, N)
     inside = np.sum(x**2 + y**2 <= 1)
-    return 4*(inside/N)
+    return 4 * (inside / N)
 
-N = int(input("enter number of samples: "))
-print("estimated pi:", monte_carlo_pi(N))
+while True:
+    N = int(input("Enter number of samples: (0 to exit)"))
+    if N == 0:
+        break
+    print("Estimated pi:", monte_carlo_pi(N))
